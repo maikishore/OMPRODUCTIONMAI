9ac18f5a00f92671050f207d345cee63ac7c6f31
maikishore <hello@getmaibrain.com>
2024-07-03 22:09:13 +0530

    OM

---
+++ app/pages/main/page.tsx
@@ -0,0 +1,15 @@
+import type { NextPage } from 'next'
+
+const Home: NextPage = () => {
+  return (
+    <div className="flex flex-col justify-center items-center h-screen">
+        <h1 className='text-6xl font-bold'>OMSRISAIRAM</h1>
+        <br/>
+      <h1 className="text-4xl font-bold">Hello, Next.js with Tailwind CSS!</h1>
+
+  
+    </div>
+  )
+}
+
+export default Home
